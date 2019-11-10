e5d62701bdf76356b2a27f33d7bd8fb043169a87
Haoranwugithub <Haoranwugithub@users.noreply.github.com>
2019-11-09 17:54:54 -0800

    dele lib and bin

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Wu/Desktop/UWB_CSS342_2019_Fall/UWB_CSS342_2019_Fall/class6/homework/problem_3
-# Build directory: /Users/Wu/Desktop/UWB_CSS342_2019_Fall/UWB_CSS342_2019_Fall/class6/homework/problem_3/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../lib/googletest/googletest-build")
-subdirs("test")
--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Wu/Desktop/UWB_CSS342_2019_Fall/UWB_CSS342_2019_Fall/class6/homework/problem_3/test
-# Build directory: /Users/Wu/Desktop/UWB_CSS342_2019_Fall/UWB_CSS342_2019_Fall/class6/homework/problem_3/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit "/Users/Wu/Desktop/UWB_CSS342_2019_Fall/UWB_CSS342_2019_Fall/class6/homework/problem_3/cmake-build-debug/unit_test")
-set_tests_properties(unit PROPERTIES  _BACKTRACE_TRIPLES "/Users/Wu/Desktop/UWB_CSS342_2019_Fall/UWB_CSS342_2019_Fall/class6/homework/problem_3/test/CMakeLists.txt;12;add_test;/Users/Wu/Desktop/UWB_CSS342_2019_Fall/UWB_CSS342_2019_Fall/class6/homework/problem_3/test/CMakeLists.txt;0;")
